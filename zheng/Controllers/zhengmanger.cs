85811478fb2a4ee295ee31debcbe93ef98447e63
nitzheng <2279590115@qq.com>
2017-06-16 10:19:59 +0800

    11

--- zheng/Controllers/zhengmanger.cs
+++ zheng/Controllers/zhengmanger.cs
@@ -5,12 +5,16 @@ using System.Text;
 using System.Threading.Tasks;
 using test.modle;
 using test;
+using zheng.Dao;
+
 
 namespace test.manger
 {
-    class zhengmanger
+    class zhengmanger//:IUserDao
     {
-
+        //public int putUserAge(int age)
+     //   {
+     //   }
         public IList<test.modle.zheng> GetAllData()
         {
             using (var session = NHibernateHelper.OpenSession())
@@ -26,9 +30,8 @@ namespace test.manger
         static void Main(string[] args)
         {
             zhengmanger pc = new zhengmanger();
-            IList<test.modle.zheng> apc = pc.GetAllData();
-
-            Console.WriteLine("开始测试查询功能!");
+            IList<test.modle.zheng> apc = pc.GetAllData();            
+            Console.WriteLine("开始测试查询功能!!");
             foreach (test.modle.zheng cp in apc)
             {
                 Console.WriteLine(cp.Name);//输出查询到的所有表中元素的名字  
